ee98bdf8bf257ee2c3ba3ce903dbd113c995dbdc
Zack Fons <nofcaz@protonmail.com>
2023-11-08 13:07:06 -0500

    Updated README and added new functional buttons

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # todo-app
 
-This is a basic todo list built with just HTML, CSS, and JavaScript, which stores the user's list in local storage for persistent functionality and auto-sorts todo items alphabetically.
+This is a basic todo list built with just HTML, CSS, and JavaScript, which stores the user's list in local storage for persistent functionality and auto-sorts todo items alphanumerically.
 
-# Basic functionality in action
+# Creating a new task
 
 ![Creating new tasks](<creating new tasks.gif>)
+
+# Removing a newly created task
+
+# Removing a completed task
+
+# Clearing the todo list (clears local storage and reloads the page)
+
+# Under the hood (what happens in local storage when creating/removing/updating tasks)
+
+# Roadmap
+
+- Adding a prompt to the Clear Task List button to ensure user definitely wants to reset their todo list
+- Adding a calendar to the app
+- Applying tasks to user-selected calendar days
+- Storing todo lists in an external database (to not rely on local storage)
--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 const itemContainer = document.getElementById("items");
 const itemTemplate = document.getElementById("itemTemplate");
 const addButton = document.getElementById("add");
+const removeNewButton = document.getElementById("remove-new");
+const removeCompletedButton = document.getElementById("remove-completed");
+const clearButton = document.getElementById("clear");
 
 
 // access localStorage window property to either get the existing todo item or return an empty array if there's no existing items, such as for a fresh todo list
 function getItems() {
-    const value = localStorage.getItem("todo") || "[]";
+    const value = localStorage.getItem("todo-list") || "[]";
     return JSON.parse(value);
 }
 
-// store the todo items from getItems in an accessible variable for setItems
+// store the todo items from getItems in an accessible variable
 let items = getItems();
 
 // convert the JSON object returned from getItem into a javascript object and set the object into local storage for easy access/manipulation
 function setItems(items) {
     const itemsJson = JSON.stringify(items);
-    localStorage.setItem("todo", itemsJson);
+    localStorage.setItem("todo-list", itemsJson);
 }
 
 // update the task item to save the user input data for each one
@@ -25,7 +28,7 @@ function updateItem(item, key, value) {
     refreshList();
 }
 
-// add a new todo task at the top of the list with Array.unshift, then set the new item to local storage, and then re-render the task list
+// add a new todo task at the top of the list with Array.unshift, then set the new items list to local storage, and then re-render the task list
 function addItem() {
     items.unshift({
         description: "",
@@ -35,7 +38,32 @@ function addItem() {
     refreshList();
 }
 
-// re-render the new task list
+// remove the most recently added item in the todo list with Array.shift, then set the new items list to local storage, and then re-render the task list
+function removeNewItem() {
+    items.shift();
+    setItems(items);
+    refreshList();
+}
+
+// loop through the todo list items and remove completed items, one at a time, with Array.pop, then set the new items list to local storage, and then re-render the task list
+// unexpected behavior: if more than two items are completed, e.g. 3 or more, this fn removes two completed items at once
+function removeCompletedItem() {
+    for (let item of items) {
+        if (item.completed) {
+            items.pop();
+        }
+    }
+    setItems(items);
+    refreshList();
+}
+
+// clear the todo object from local storage to clear the entire list, refresh the page to start a new list
+function clearList() {
+    localStorage.removeItem("todo-list");
+    location.reload();
+}
+
+// re-render the task list
 // cloneNode(true) indicates a deep clone where we want the entire node and subtree of child nodes to ensure the entire template is copied
 function refreshList() {
     items.sort((a, b) => { // basic sorting algorithm to get alphabetical sorting of tasks by description, updating on task completion
@@ -47,6 +75,7 @@ function refreshList() {
         }
         return a.description < b.description ? -1 : 1;
     });
+    setItems(items);
     itemContainer.innerHTML = ""; // removing this line duplicates the full todo item list on add button click?
     for (let item of items) { // loop through the tasks from getItems
         const itemElement = itemTemplate.content.cloneNode(true); // copy the "content" of the item template div, being the "item" div
@@ -65,5 +94,8 @@ function refreshList() {
 }
 
 addButton.addEventListener("click", () => { addItem(); });
+removeNewButton.addEventListener("click", () => { removeNewItem(); });
+removeCompletedButton.addEventListener("click", () => { removeCompletedItem(); });
+clearButton.addEventListener("click", () => { clearList(); });
 
 refreshList();
